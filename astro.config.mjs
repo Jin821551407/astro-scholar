aeda0805a8351847426e9b25f71968fb07540db1
lijingyu <18683257353@163.com>
2026-05-30 23:20:11 +0800

    添加部署脚本

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://whydevils.github.io',
-  base: '/astro-scholar/',
+  site: 'https://jin821551407.github.io',
+  base: '/',
 });
